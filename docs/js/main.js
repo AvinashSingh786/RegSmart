49f3f388ce757e60614ba7cc9543533f06d8324f
AvinashSingh786 <u14043778@tuks.co.za>
2017-08-11 10:52:34 +0200

    formspree

--- docs/js/main.js
+++ docs/js/main.js
@@ -8,14 +8,14 @@ jQuery(function($) {
 	});
 
 	//Ajax contact
-	var form = $('.contact-form');
-	form.submit(function () {
-		$this = $(this);
-		$.post($(this).attr('action'), function(data) {
-			$this.prev().text(data.message).fadeIn().delay(3000).fadeOut();
-		},'json');
-		return false;
-	});
+	// var form = $('.contact-form');
+	// form.submit(function () {
+	// 	$this = $(this);
+	// 	$.post($(this).attr('action'), function(data) {
+	// 		$this.prev().text(data.message).fadeIn().delay(3000).fadeOut();
+	// 	},'json');
+	// 	return false;
+	// });
 
 	//smooth scroll
 	$('.navbar-nav > li').click(function(event) {
